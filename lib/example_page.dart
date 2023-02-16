02d05225cfec4b3980331400e6136cedd802b0d2
David Green <greensopinion@gmail.com>
2023-02-15 19:47:19 -0800

    clear cache between examples

--- lib/example_page.dart
+++ lib/example_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:vector_map_examples/loadable.dart';
 
 import 'model.dart';
 
@@ -11,5 +15,15 @@ class ExamplePage extends StatelessWidget {
       appBar: AppBar(title: Text(example.name)),
       body: SafeArea(child: _mainContent(context)));
 
-  Widget _mainContent(BuildContext context) => example.builder(context);
+  Widget _mainContent(BuildContext context) => Loadable(
+      loader: _clearCache, builder: (context, _) => example.builder(context));
+}
+
+Future _clearCache() async {
+  final tmp = await getTemporaryDirectory();
+  final tilesDir = Directory('${tmp.path}/.vector_map');
+  if (await tilesDir.exists()) {
+    await tilesDir.delete(recursive: true);
+  }
+  return true;
 }
