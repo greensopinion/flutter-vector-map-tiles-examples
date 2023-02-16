5a0c82568bc2d4c68cea6e887032acaf57417dd6
David Green <greensopinion@gmail.com>
2023-02-15 19:40:44 -0800

    simplify example page

--- lib/example_page.dart
+++ lib/example_page.dart
@@ -2,24 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'model.dart';
 
-class ExamplePage extends StatefulWidget {
+class ExamplePage extends StatelessWidget {
   final ExampleModel example;
   const ExamplePage({super.key, required this.example});
 
   @override
-  State<StatefulWidget> createState() => _ExamplePage();
-}
-
-class _ExamplePage extends State<ExamplePage> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: Text(widget.example.name)),
+      appBar: AppBar(title: Text(example.name)),
       body: SafeArea(child: _mainContent(context)));
 
-  Widget _mainContent(BuildContext context) => widget.example.builder(context);
+  Widget _mainContent(BuildContext context) => example.builder(context);
 }
