2cf264ca866f7a8428699634b2d3723869a39de6
David Green <greensopinion@gmail.com>
2023-03-19 16:18:41 -0700

    add raster experiment

--- lib/map.dart
+++ lib/map.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:vector_map_tiles/vector_map_tiles.dart';
+import 'package:vector_map_tiles/experimental.dart';
 
 class MapWidget extends StatefulWidget {
   final VectorTileLayer Function(BuildContext) layerFactory;
@@ -20,29 +21,40 @@ class _MapWidget extends State<MapWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Expanded(
-          child: FlutterMap(
-        mapController: _controller,
-        options: MapOptions(
-            center: widget.center ?? LatLng(43.7763331, 7.4733097),
-            zoom: widget.zoom ?? 13,
-            maxZoom: 22,
-            interactiveFlags: InteractiveFlag.drag |
-                InteractiveFlag.flingAnimation |
-                InteractiveFlag.pinchMove |
-                InteractiveFlag.pinchZoom |
-                InteractiveFlag.doubleTapZoom),
-        children: [widget.layerFactory(context)],
-      )),
-      Wrap(alignment: WrapAlignment.spaceBetween, children: [
-        _positionButton('London', 51.515556, -0.093056),
-        _positionButton('Paris', 48.8566, 2.3522),
-        _positionButton('San Francisco', 37.7749, -122.4194),
-        _positionButton('Vancouver', 49.2827, -123.1207),
-        _positionButton('Hong Kong', 22.3193, 114.1694)
-      ])
-    ]);
+    final layer = widget.layerFactory(context);
+    return RasterTileState(
+        tileProviders: layer.tileProviders,
+        theme: layer.theme,
+        concurrency: layer.concurrency,
+        fileCacheMaximumSizeInBytes: layer.fileCacheMaximumSizeInBytes,
+        fileCacheTtl: layer.fileCacheTtl,
+        memoryTileCacheMaxSize: layer.memoryTileCacheMaxSize,
+        memoryTileDataCacheMaxSize: layer.memoryTileCacheMaxSize,
+        textCacheMaxSize: layer.textCacheMaxSize,
+        tileDelay: layer.tileDelay,
+        builder: (context, tileProvider) => Column(children: [
+              Expanded(
+                  child: FlutterMap(
+                mapController: _controller,
+                options: MapOptions(
+                    center: widget.center ?? LatLng(43.7763331, 7.4733097),
+                    zoom: widget.zoom ?? 13,
+                    maxZoom: 22,
+                    interactiveFlags: InteractiveFlag.drag |
+                        InteractiveFlag.flingAnimation |
+                        InteractiveFlag.pinchMove |
+                        InteractiveFlag.pinchZoom |
+                        InteractiveFlag.doubleTapZoom),
+                children: [TileLayer(tileProvider: tileProvider)],
+              )),
+              Wrap(alignment: WrapAlignment.spaceBetween, children: [
+                _positionButton('London', 51.515556, -0.093056),
+                _positionButton('Paris', 48.8566, 2.3522),
+                _positionButton('San Francisco', 37.7749, -122.4194),
+                _positionButton('Vancouver', 49.2827, -123.1207),
+                _positionButton('Hong Kong', 22.3193, 114.1694)
+              ])
+            ]));
   }
 
   Widget _positionButton(String name, double latitude, double longitude) {
